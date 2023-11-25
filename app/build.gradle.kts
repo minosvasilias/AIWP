86df3eb45dc17be19845780104f5a1f7fbf1478a
Markus <44006014+minosvasilias@users.noreply.github.com>
2023-11-25 16:24:19 +0100

    Increment version to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.minosapps.aiwp"
         minSdk = 24
         targetSdk = 33
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
